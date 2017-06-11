01ba9f9c9fafe5ec24738cfad4b852d24e1177a1
Ryan Hoyda <Ryanhoyda@gmail.com>
2017-06-10 21:00:17 -0700

    turn selector

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,39 +22,74 @@ firebase.initializeApp(config);
 function updatePlayerName(selector, name, group){
     //todo: change the text in selector to equal name
     $(selector).text(name);  //what do we do here to make to replace 'waiting for player x' with name?
-    $( group + ' > li' ).removeClass( "hidden" );
+    $( group + ' > li' ).removeClass( "hidden" ); //remove css class hidden
 }
 
 
+//function to announce player
+function announcePlayer(myPlayer, playerNum){
+    //Remove all html, button etc in order to replace with text
+    $('#messageRow').empty();
 
-function announcePlayer(myPlayer){
-    //Remove all html
-    $('#messages').empty();
-    //then insert html that reads Hi! [myPlayer.name] you are [player one/two]
-    //create a variable that holds the text
+    //creating two bootstrap rows.  Probably could have created a well
+    var html = '<div class="row"><div class="col-xs-offset-5 col-sm-offset-5 col-md-offset-5 col-lg-offset-5"><span id="playerName"></span></div></div> <div class="row"><div class="col-xs-offset-5 col-sm-offset-5 col-md-offset-5 col-lg-offset-5"><span id="message"></span></div></div>'
+    $('#messageRow').html(html);
+
     var playerMessages = ("Hi " + myPlayer.name + " You are Player " + playerNum);
 
-    //insert html into #messages with your string
-    $('#messages').html(playerMessages);
+    $('#playerName').text(playerMessages);
 }
 
+
+//turn switching
+//essentially have 4 browser windows, 2 in each tab
+function runPlayerTurn(currentPlayer, p1, p2, database){
+
+    //check if both players have made moves
+    if(p1.move != '' && p2.move != ''){
+        //check who wins
+        //update wins/loss
+        //reset moves to ''
+        //push to db
+    } else if (currentPlayer == myPlayer && myPlayer == p1){
+
+    } else if (currentPlayer != myPlayer && myPlayer == p1){
+
+    } else if (currentPlayer == myPlayer && myPlayer == p2){
+
+    } else if (currentPlayer != myPlayer && myPlayer == p2){
+
+    }
+}
+
+//check for winner
+function checkWinner(p1, p2){
+
+
+}
 
 
 //document. ready function to start
-
 $(document).ready(function(){
     startFirebase();
 
+
     //Define variables here
-    //Define variables here
+    //objects with properties
     var playerOne = {
         name: "",
-        move: ""
+        move: "",
+        playerNumber: 1,
+        wins: 0,
+        lose: 0
     };
 
     var playerTwo = {
         name: "",
-        move: ""
+        move: "",
+        playerNumber: 2,
+        wins: 0,
+        lose: 0
     };
 
     var myPlayer;
@@ -68,8 +103,10 @@ $(document).ready(function(){
         if(playerOne.name == '') {
             //set playerOne to name in firebase db
             database.ref().set({
-                playerOne:name, //set to the value in the textbox
-                playerTwo: playerTwo.name
+                //important to establish both objects in each statement as to not overwrite
+                //initial issue - overwriting player 1 in Firebase
+                playerOne:name, //set to the value in the textbox in html
+                playerTwo: playerTwo.name //set the value to the object (line 60)
             });
             myPlayer = playerOne;
             announcePlayer(myPlayer, 1);
@@ -79,8 +116,8 @@ $(document).ready(function(){
         } else if (playerTwo.name == ''){
             //set playerTwo to name in firebase db
             database.ref().set({
-                playerOne: playerOne.name,
-                playerTwo: name //set to the value in the textbox
+                playerOne: playerOne.name, //set the value to the object (line 55)
+                playerTwo: name //set to the value in the textbox in html
             });
             myPlayer = playerTwo;
             announcePlayer(myPlayer, 2);
@@ -94,10 +131,10 @@ $(document).ready(function(){
         //get input box "info"
         var text = $("#inputID").val();
         console.log(text);
-
     });
 
 
+    //function to remove player names and reset to blank strings in firebase
     $(window).on('unload', function(){
         //reset playerOne and playerTwo to blank strings
         if (myPlayer == playerOne){
@@ -114,7 +151,7 @@ $(document).ready(function(){
     });
 
 
-
+    //define firebase as database
     var database = firebase.database();
 
     database.ref().on("value", function(snapshot) {
@@ -128,8 +165,30 @@ $(document).ready(function(){
             updatePlayerName('#playerTwoName', playerTwo.name, '#playerTwo');
         }
 
-    });
+        //define currentPlayer variable
+        //both clients need to understand whose turn it is
+        //currentPlayer will either be player1 or player2
 
+        var currentPlayer = snapshot.val().currentPlayer;
+        var p1 = snapshot.val().p1;
+        var p2 = snapshot.val().p2;
+
+        if (currentPlayer == undefined){
+            if(playerOne.name != '' && playerTwo.name != ''){
+                database.ref().set({
+                    playerOne: playerOne.name,
+                    playerTwo: playerTwo.name,
+                    currentPlayer: playerOne,
+                    p1: playerOne,
+                    p2: playerTwo
+                });
+            }
+        } else {
+            if(playerOne.name != '' && playerTwo.name != ''){
+                runPlayerTurn(currentPlayer, myPlayer, p1, p2, database);
+            }
+        }
+    });
 });
 
 
