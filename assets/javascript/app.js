f9d8e96aaa32f599d44753d0b30e8afd72fd15cd
Ryan Hoyda <Ryanhoyda@gmail.com>
2017-06-10 19:26:01 -0700

    writes players to db.  update players in html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,50 +19,60 @@ firebase.initializeApp(config);
 }
 
 
+function updatePlayerName(selector, name){
+    //todo: change the text in selector to equal name
+    $(selector).text(name);  //what do we do here to make to replace 'waiting for player x' with name?
+    $( ".list-group-item" ).removeClass( "hidden" );
+}
+
 
 //document. ready function to start
 
 $(document).ready(function(){
     startFirebase();
 
-    var playerOne= {
+    //Define variables here
+    //Define variables here
+    var playerOne = {
         name: "",
         move: ""
-    }
+    };
 
-
-    var playerTwo= {
+    var playerTwo = {
         name: "",
         move: ""
-    }
+    };
 
+    var myPlayer;
 
 
 //implement click listener
     $('#start').on('click', function() {
-        console.log('clicked');
-        //get input box "name"
         var name = $("#name").val();
-
-
 
         //check if playerOne.name is blank
         if(playerOne.name == '') {
             //set playerOne to name in firebase db
             database.ref().set({
-                playerOne:name
+                playerOne:name, //set to the value in the textbox
+                playerTwo: playerTwo.name
             });
-            return;
+            myPlayer = playerOne;
+            updatePlayerName('#playerOneName', name);
+
 
         } else if (playerTwo.name == ''){
             //set playerTwo to name in firebase db
             database.ref().set({
-                playerTwo:name
+                playerOne: playerOne.name,
+                playerTwo: name //set to the value in the textbox
             });
-            return;
+            myPlayer = playerTwo;
+            updatePlayerName('#playerTwoName', name);
         }
 
     });
+
 
     $('#send').on('click', function() {
         console.log('send');
@@ -73,10 +83,22 @@ $(document).ready(function(){
     });
 
 
-    $(document).unload(function(){
+    $(window).on('unload', function(){
         //reset playerOne and playerTwo to blank strings
-
+        if (myPlayer == playerOne){
+            database.ref().set({
+                playerOne: "",
+                playerTwo: playerTwo.name
+            });
+        } else if (myPlayer == playerTwo){
+            database.ref().set({
+                playerOne: playerOne.name,
+                playerTwo: ""
+            })
+        }
     });
+
+
 
     var database = firebase.database();
 
